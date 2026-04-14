2a41f137f8acf41a007901960f3c779a50e9ccd0
TrNgyn <minhtran.nguyentrinh@gmail.com>
2026-04-14 10:10:32 +1000

    Terraform infra

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Unique identifier for global bucket naming
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# Landing Zone: The "Bronze" layer for raw data ingestion
+resource "aws_s3_bucket" "olist_landing" {
+  bucket = "olist-maas-landing-${random_id.bucket_suffix.hex}"
+}
+
+# Security: Prevent public access to the Landing Zone
+resource "aws_s3_bucket_public_access_block" "landing_security" {
+  bucket                  = aws_s3_bucket.olist_landing.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Ingestion Folders: Partitioning data by business domain
+resource "aws_s3_object" "ecommerce_folder" {
+  bucket = aws_s3_bucket.olist_landing.id
+  key    = "raw/ecommerce/"
+}
+
+resource "aws_s3_object" "marketing_folder" {
+  bucket = aws_s3_bucket.olist_landing.id
+  key    = "raw/marketing/"
+}
+
+resource "aws_s3_object" "ab_testing_folder" {
+  bucket = aws_s3_bucket.olist_landing.id
+  key    = "raw/ab_testing/"
+}
+
+# The bucket name needed for data syncing
+output "landing_bucket_name" {
+  value = aws_s3_bucket.olist_landing.bucket
+}
